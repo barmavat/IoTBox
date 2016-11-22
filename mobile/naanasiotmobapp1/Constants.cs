deb5d4fd930742e2a2b4f6d7b6c3d474bf6130a3
Balaji <barmavat@gmail.com>
2016-11-22 12:25:41 +0530

    First Version

---
+++ mobile/naanasiotmobapp1/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace naanasiotmobapp1
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://naanasiotmobapp1.azurewebsites.net";
+	}
+}
+
